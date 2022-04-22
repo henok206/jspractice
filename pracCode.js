01b9cda938ea229cc649de23cba89f87c1df05cf
Henok <henok_206@yahoo.com>
2022-04-22 17:34:23 +0300

    pro init

--- pracCode.js
+++ pracCode.js
@@ -219,11 +219,11 @@
 //   document.write(task,time,compleated+"<br>");
 // 
 //convert arry to object
-const students = [
-  ['David', ['HTM', 'CSS', 'JS', 'React'], [98, 85, 90, 95]],
-  ['John', ['HTM', 'CSS', 'JS', 'React'], [85, 80, 85, 80]]
-];
-console.log(students);
+// const students = [
+//   ['David', ['HTM', 'CSS', 'JS', 'React'], [98, 85, 90, 95]],
+//   ['John', ['HTM', 'CSS', 'JS', 'React'], [85, 80, 85, 80]]
+// ];
+// console.log(students);
 // let conToObject=({},students);
 // console.log(conToObject);
 
